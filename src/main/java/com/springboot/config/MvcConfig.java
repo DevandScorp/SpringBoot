82dc2fd94d6739e66b038210086c34e3d0448862
DevandScorp <artemda459@gmail.com>
2018-08-11 14:17:44 +0300

    #10-Файлы(похоже,что Spring Boot по умолчанию юзает какие-то томкатовские директории,из которых нужно было выйти).

--- src/main/java/com/springboot/config/MvcConfig.java
+++ src/main/java/com/springboot/config/MvcConfig.java
@@ -1,19 +1,34 @@
 package com.springboot.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                /**file://-это протокол*/
+                .addResourceLocations("file://" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 
     public void addViewControllers(ViewControllerRegistry registry) {
+
         /**
          * По сути это-маппинг твоих контроллеров.Тут для /login в соответствие ставится шаблон login
          * Т.е. по сути как return ViewName
          */
         registry.addViewController("/login").setViewName("login");
+
     }
 
 }
--- src/main/resources/templates/main.ftl
+++ src/main/resources/templates/main.ftl
@@ -7,16 +7,17 @@
     </div>
     <div class="text-center">${sometext}</div>
     <div>
-        <form method="post">
+        <form method="post" enctype = "multipart/form-data">
             <input type = "hidden" name = "_csrf" value = "${_csrf.token}"/>
             <input type="text" name="text" placeholder="Введите сообщение" />
+            <input type="file" name = "file"/>
             <input type="text" name="tag" placeholder="Тэг">
             <button type="submit">Добавить</button>
         </form>
     </div>
     <form method="get" action="/main">
         <!--Вот эти скрытые поля нужны только для post-запросов-->
-        <input type="text" name="filter">
+        <input type="text" name="${filter?ifExists}">
         <button type="submit">Найти</button>
     </form>
     <div>Список сообщений</div>
@@ -25,6 +26,11 @@
         <span>${message.text}</span>
         <i>${message.tag}</i>
         <strong>${message.author}</strong>
+        <div>
+            <#if message.filename??>
+                <img src="/img/${message.filename}" alt="">
+            </#if>
+        </div>
     <#else >
     No message
     </#list>
